5ee9c22177ec6bd276234d77e86ed77e9efb02b1
Johnson Li <johnsonli1993@gmail.com>
2019-01-20 08:26:27 +0800

    more log

--- examples/server.cc
+++ examples/server.cc
@@ -1840,11 +1840,16 @@ int Server::on_read(int fd) {
     return 0;
   }
 
-  std::cerr << "update fd: " << fd << std::endl;
   h->update_fd(fd);
   auto remote_addr = h->remote_addr();
   remote_addr.len = addrlen;
   memcpy(&remote_addr.su.sa, &su.sa, addrlen);
+  char str[INET_ADDRSTRLEN];
+  inet_ntop(AF_INET, &(remote_addr.su.in.sin_addr), str, INET_ADDRSTRLEN);
+  std::cerr << "update fd: " << fd << ", " << str << ":" << ntohs(remote_addr.su.in.sin_port) << std::endl;
+  remote_addr = h->remote_addr();
+  inet_ntop(AF_INET, &(remote_addr.su.in.sin_addr), str, INET_ADDRSTRLEN);
+  std::cerr << "update fd: " << fd << ", " << str << ":" << ntohs(remote_addr.su.in.sin_port) << std::endl;
   rv = h->on_read(buf.data(), nread);
   if (rv != 0) {
     if (rv != NETWORK_ERR_CLOSE_WAIT) {
